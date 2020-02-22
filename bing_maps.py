2ff93ffbcd7353b5c144b2370765a88897b8a4d4
Goochy12 <liamgooch@live.com.au>
2020-02-22 19:16:25 +1100

    reformatted

--- bing_maps.py
+++ bing_maps.py
@@ -1,29 +1,26 @@
-key = "AhoMw4Yz8HJfzN9iUL1v3i0nA9Nn8SNJudyR_IkxnqcnjYX_sbQn8XZUe2qL9_gO"
 import requests
 
+key = "AhoMw4Yz8HJfzN9iUL1v3i0nA9Nn8SNJudyR_IkxnqcnjYX_sbQn8XZUe2qL9_gO"
 routeURL_synchronous = "http://dev.virtualearth.net/REST/V1/Routes/Driving?"
 
 
-# latLongList = [[-34.210721,142.0635644],[-37.8015951,144.8645087]]
-
-# url = routeURL_synchronous + "wp.0="+ str(latLongList[0][0]) + "," + str(latLongList[0][1]) + "&wp.1=" + str(latLongList[1][0]) + "," + str(latLongList[1][1]) + "&key="+key
-# print(url)
-
-# route = requests.get(url)
-# routeJSON = route.json()
-# travelDuration = routeJSON["resourceSets"][0]["resources"][0]["travelDistance"]
-# print(travelDuration)
-
 def returnRequestJSON(request):
+    """
+    Method to return the JSON (python dict) of a request
+    :param request: the HTTP request (GET)
+    :return: JSON (python dict)
+    """
     return request.json()
+
 
 def makeRouteRequest(latLongList):
     """
     Method to make a GET request to the bing maps API
+    # wp.x indicates a way point
     :return: route - the request in its raw format
     """
+    global key
     # construct the URL
-    # wp.x indicates a waypoint
     url = routeURL_synchronous + "wp.0=" + str(latLongList[0][0]) + "," + str(latLongList[0][1]) + "&wp.1=" + str(
         latLongList[1][0]) + "," + str(latLongList[1][1]) + "&key=" + key
 
