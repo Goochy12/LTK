ce36baabf9f02ed01668d4458278915af6f786ca
Goochy12 <liamgooch@live.com.au>
2020-02-22 23:12:33 +1100

    updated test imports

--- tests.py
+++ tests.py
@@ -1,15 +1,18 @@
 import unittest
-import main
+import JDAssistant
 
 class MyTestCase(unittest.TestCase):
-    def test_fileCreation(self):
-        main.createOutputFile()
-        f = open("output.txt","r")
-        # self.assertTrue(f.readlines()=="")
-        # self.assertFalse(f.readlines()=="")
+    # def test_fileCreation(self):
+    #     main.createOutputFile()
+    #     f = open("output.txt","r")
+    #     # self.assertTrue(f.readlines()=="")
+    #     # self.assertFalse(f.readlines()=="")
+    #
+    # def test_timeDistChecker(self):
+    #     main.timeDistChecking()
 
-    def test_timeDistChecker(self):
-        main.timeDistChecking()
+    def test_geocodeChecker(self):
+        JDAssistant.geocoding()
 
 
 if __name__ == '__main__':
