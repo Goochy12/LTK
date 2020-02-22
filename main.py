28efc2664826e14854dba9c6ed317b8369cf309b
Goochy12 <liamgooch@live.com.au>
2020-02-22 19:06:09 +1100

    finalised time/dist checking and file reading, creating and parsing methods

--- main.py
+++ main.py
@@ -3,40 +3,112 @@
 # script to help with manual JDA tasks
 #   - Checking travel time
 #   - Matching addresses to lat longs
-# uses Google Maps API
-#   - key: AIzaSyAko7wydJJaoWU313XaWd9fgr2Idunz9uk
 
 import os
-# import google
+import bing_maps
+
+fileName = "output.txt"
+
 
 def getFile():
     """
     Method to read the import file
-    :return: fileName - the name of the file
+    :return: importFilePath - the path to the file
     """
-    fileName = input("Please enter the path to the import file: ")  # get the file path
-    return fileName
+    importFilePath = input("Please enter the path to the import file: ")  # get the file path
+    return importFilePath  # return importFilePath
 
 
-def parseTimeCheckingFile(fileName):
+def createOutputFile():
+    """
+    Method to create an output file
+    :return: None
+    """
+    global fileName  # get the global output file name
+    f = open(fileName, "w")  # create the file
+    f.close()  # close the file
     return
 
 
-def timeChecking():
+def writeToFile(line):
     """
-    Method to check how long it takes to get from an Origin point to a Destination point.
+    Method to write lines to a file
+    :param line: a line to write to file
+    :return: None
+    """
+    global fileName  # get the global output file name
+    f = open(fileName, "a")  # open the file
+    f.write(line)  # append the line to the file
+    return
+
+
+def parseTimeDistanceCheckingFile(filePath):
+    """
+    Method to parse the file in the format for checking time and distance
+    :param filePath: The path to the input file
+    :return: latLongList - a list of origin and destination points
+    """
+    f = open(filePath, "r")  # open the input file
+    latLongList = []  # initalise the lat and long list
+    # iterate through the lines in the file
+    for eachLine in f:
+        r = eachLine.split(" ")  # split each line by a space -> origin_lat origin_long dest_lat dest_long
+        # iterate through each coordinate and convert to a float
+        for i in range(len(r)):
+            r[i] = float(r[i])  # convert to a float
+        origin = [r[0], r[1]]  # origin coordinates
+        dest = [r[2], r[3]]  # destination coordinates
+
+        latLongList.append([origin, dest])  # append the coordinates
+    return latLongList  # return the list of lats and longs
+
+
+def timeDistChecking():
+    """
+    Method to get time and distance from an Origin point to a Destination point.
     Uses a list of points
     :return: None
     """
-    file = getFile() # get input file
-    parseTimeCheckingFile(file)
-    # parse into array
+    filePath = getFile()  # get input file
+
+    latLongList = parseTimeDistanceCheckingFile(filePath)  # parse into array
+    # latLongList = [[[-34.210721, 142.0635644], [-37.8015951, 144.8645087]]]
+
+    createOutputFile()  # create a new output file
+    message = "Routes Created Successfully!"  # create a success message
+
     # match against maps
-    # write to file
+    for eachCoord in latLongList:
+        route = bing_maps.makeRouteRequest(eachCoord)  # make the route request
+        routeJSON = bing_maps.returnRequestJSON(route)  # convert response to JSON (python dict)
+
+        travelDistance = routeJSON["resourceSets"][0]["resources"][0]["travelDistance"]  # get the distance
+        travelDuration = routeJSON["resourceSets"][0]["resources"][0]["travelDuration"]  # get the duration
+
+        # write to file - origin lat long -> dest lat long | time distance
+        outputLine = str(eachCoord[0][0]) + "/" + str(eachCoord[0][1]) + " -> " + str(eachCoord[1][0]) + "/" + str(
+            eachCoord[1][1]) + " | " + str(travelDuration) + ", " + str(travelDistance)
+        writeToFile(outputLine)  # write line to file
+
+    printMessage(message)
     return
+
 
 def geocoding():
     return
+
+
+def printMessage(message):
+    """
+    Method to print a message to the user
+    :return:
+    """
+    os.system("cls")  # clear the console
+    print(message)  # print the message
+    print()
+
+    return
+
 
 def run():
     """
@@ -44,7 +116,7 @@ def run():
     :return: None
     """
     os.system("cls")  # wipe the system screen
-    exitCode = 3    # code user enters to exit
+    exitCode = 3  # code user enters to exit
 
     print("Welcome to JDAssistance!")  # print welcome message
     selection = ""  # initialise user selection
@@ -65,7 +137,7 @@ def run():
             selection = int(input())
 
         if selection == 1:
-            timeChecking()  # Time Checking
+            timeDistChecking()  # Time Checking
         elif selection == 2:
             geocoding()  # Geocoding (Address -> Lat/Long)
         else:
