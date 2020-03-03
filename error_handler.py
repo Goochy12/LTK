cd0b9031d89d1dd2521a1ff9471633a60c46ed3b
Liam <lgoo0003@student.monash.edu>
2020-03-03 19:49:13 +1100

    Update error_handler.py

--- error_handler.py
+++ error_handler.py
@@ -4,7 +4,7 @@ import os
 
 def checkDirectoryExists(dir):
     if not os.path.isdir(dir):
-        raise Exception("This directory does not exist! Would you like it to be created (y/n)?")
+        raise Exception("This directory does not exist!")
     return os.path.isdir(dir)
 
 def checkFileExists(dir):
