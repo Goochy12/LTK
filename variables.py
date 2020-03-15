1d96c45478a9219a96d99b1d51540cbf75af0966
Liam-Desktop <liamgooch@live.com.au>
2020-03-15 18:19:03 +1100

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,3 +1,3 @@
-applicationName = "LTK (Liam's TookKit)"  # name of the application
-
+applicationName = "LTK (Liam's ToolKit)"  # name of the application
+featureNames = ["Travel Time Checking", "Geocoding"]  # features in the application
 bingMapsAPIKey = "AhoMw4Yz8HJfzN9iUL1v3i0nA9Nn8SNJudyR_IkxnqcnjYX_sbQn8XZUe2qL9_gO"  # bing maps API key
