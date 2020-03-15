8f1fee5089a168e2cc5be81c682a8aaefa792594
Liam-Desktop <liamgooch@live.com.au>
2020-03-15 21:56:11 +1100

    set file names and paths

--- gui/input_output_gui.py
+++ gui/input_output_gui.py
@@ -1,12 +1,29 @@
 from tkinter import *
 from tkinter import filedialog
+from features import travel_time_checker
 
 class InputOutputGui:
     def __init__(self, feature):
         self.master = feature[0]
         self.title = feature[1]
 
+        self.inputFile = None
+        self.outputFileName = None
+        self.outputFilelocation = None
+
         self.createLayout(self.master)
+
+    def setInputFile(self, f):
+        self.inputFile = f
+        return
+
+    def setOutputFileName(self, n):
+        self.outputFileName = n
+        return
+
+    def setOutputFileLocation(self, l):
+        self.outputFilelocation = l
+        return
 
     def createLayout(self, window):
 
@@ -25,7 +42,7 @@ class InputOutputGui:
         outputLocationButton = Button(window, text="Set Location", command=self.setOutputLocation)
         outputLocationButton.grid(row=2,column=1)
 
-        calculateButton = Button(window, text="Calculate", command=self.calculate)
+        calculateButton = Button(window, text="Calculate", command= lambda j=self.title: self.calculate(j))
         calculateButton.grid(row=3)
 
         return
@@ -33,11 +50,23 @@ class InputOutputGui:
     def getInputFile(self):
         inputFile = filedialog.askopenfilename(initialdir="/", title="Open Input File",
                                                filetypes=((".txt files", "*.txt"), ("All Files", "*.*")))
+        self.setInputFile(inputFile)
         return inputFile
 
     def setOutputLocation(self):
         outputLocation = filedialog.askdirectory(initialdir="/")
+        self.setOutputFileLocation(outputLocation)
         return outputLocation
 
-    def calculate(self):
+    def calculate(self, feature):
+        if feature == "Travel Time Checking":
+            c = travel_time_checker.TravelTimeChecker()
+            message = c.timeDistChecking(self.inputFile, self.outputFileName, self.outputFilelocation)
+
+            if message[0] == "OK":
+                # msg okay
+                return
+            else:
+                # something went wrong
+                return
         return
