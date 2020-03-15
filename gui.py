0b7cf96cf31bda1b6eb3c56288dd926f1ff91f1d
Liam-Desktop <liamgooch@live.com.au>
2020-03-15 18:20:22 +1100

    add buttons to gui

--- gui.py
+++ gui.py
@@ -2,11 +2,31 @@ from tkinter import *
 from tkinter.ttk import *
 import variables
 
+
+
 class App:
     def __init__(self, master):
         frame = Frame(master, width=400, height=400)
         master.title(variables.applicationName)
+        master.geometry("400x400")
         frame.pack()
+
+        # gui layout
+        buttonWindowList = []
+        # travelTimeCheckingWindowButton = Button(frame, text=variables.featureNames[0])  # Travel Time Checking
+        # geocodingWindowButton = Button(frame, text=variables.featureNames[1])  # Geocoding
+        #
+        # buttonWindowList.append(travelTimeCheckingWindowButton)
+        # buttonWindowList.append(geocodingWindowButton)
+        # travelTimeCheckingWindowButton.grid(column=0, row=1)
+
+        for eachFeature in variables.featureNames:
+            button = Button(frame, text=eachFeature)
+            buttonWindowList.append(button)
+
+        for eachButton in buttonWindowList:
+            eachButton.pack(side=TOP)
+
 
 
 root = Tk()
